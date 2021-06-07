7d3c98bc166d87765c23a59e1d67241a587571d1
Gabriele <gabrieleboccarusso@gmail.com>
2021-06-07 11:37:23 +0200

    added comments

--- english dictionary/code.py
+++ english dictionary/code.py
@@ -1,5 +1,14 @@
+'''
+code made as an exercise for "The Python Mega Course Build 10 Real World Applications"
+course on udemy.
+The curret code is more efficient and less spaghetti than the original in the course
+Gabriele Boccarusso 2021.06.07 (yyyy.mm.dd)
+'''
+
 from json import load
 from difflib import SequenceMatcher, get_close_matches
+# data will be the ONLY global variable because is a 5 megabyte file,
+# it would be to expensive to pass is in every function
 data = load(open("data/data.json"))
 
 def translate(w) -> str:
@@ -24,18 +33,19 @@ def main() -> int:
     loop: str = 'y'
     word: str = ''
 
+    # entering the main loop
     while(loop[0] == 'y'):
         word = input("enter a word: ")
         word = translate(word)
-        # if the word content is a list means that we 
+        # if the content of "word" is a list it means that we 
         # received a list of meanings
         if type(word) == list:
             for i in range(len(word)):
                 print("definition", i+1, ':', word[i])
         else: # otherwise we receive a simple message to display
             print(word)
-
-
+        
+        # repeting the loop or exiting the program
         loop = input("do you want to enter another word? ")
 
     return 0
