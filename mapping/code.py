a3d57f0c4928ff03e5f79c57918f23b5c48a8f16
Gabriele <gabrieleboccarusso@gmail.com>
2021-06-09 11:37:59 +0200

    added and implemented volcanoes data into the map

--- mapping/code.py
+++ mapping/code.py
@@ -1,11 +1,20 @@
 import folium
-map = folium.Map(location = [30, 50], zoom_start = 6, tiles = "stamen terrain")
+import pandas as pd 
+
+data = pd.read_csv("mapping-data/Volcanoes.txt")
+
+map = folium.Map(location = [48.7767982,-121.8109970], zoom_start = 10, tiles = "stamen terrain")
+
+lat = list(data.LAT)
+lon = list(data.LON)
+name = list(data.NAME)
 
 fg = folium.FeatureGroup(name = "main feature group")
 
-fg.add_child(folium.map.Marker(location = [30, 40], 
-                                popup = "hi", 
-                                icon = folium.Icon(color = "red")))
+for i in range(len(lat)):
+    fg.add_child(folium.map.Marker(location = [lat[i], lon[i]], 
+                                    popup = name[i], 
+                                    icon = folium.Icon(color = "red")))
 
 map.add_child(fg)
 
