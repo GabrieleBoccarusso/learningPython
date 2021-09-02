94f28d9daca97d3fc942902151448ec2199f285e
Gabriele <gabrieleboccarusso@gmail.com>
2021-09-02 08:39:38 +0200

    added basic status bar

--- image-slider-app/main.py
+++ image-slider-app/main.py
@@ -2,19 +2,19 @@ from tkinter import *
 from PIL import ImageTk, Image
 import os
 
-root = Tk()
-root.title("image viewer application")
-root.iconbitmap("favicon.ico")
-
 # create the list of images to display
 current_img = 0
 image_list = []
+length = '0'
 
 def create_image_list():
+    global length
     for entry in os.scandir("./images"):
         image_list.append(
             ImageTk.PhotoImage(Image.open("images/"+entry.name).resize((500,500)))
-        ) 
+        )
+
+    length = str(len(image_list))
 
 def load_first_image():
     global main_label
@@ -23,12 +23,21 @@ def load_first_image():
     main_label = Label(root, image=image_list[current_img])
     main_label.grid(row = 0, column = 0, columnspan = 3)
 
+def initial_status():
+    status = Label(root, text="image 1 of "+str(length), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
+root = Tk()
+root.title("image viewer application")
+root.iconbitmap("favicon.ico")
+
 main_label = Label(root, image = None, text = "loading")
 main_label.grid(row = 0, column = 0, columnspan = 3)
 
 def forward():
     global main_label
     global current_img
+    global status
 
     current_img+=1
 
@@ -39,9 +48,13 @@ def forward():
     main_label = Label(root, image=image_list[current_img])
     main_label.grid(row = 0, column = 0, columnspan = 3)
 
+    status = Label(root, text="image "+ str(current_img+1) +" of "+ length, bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 def back():
     global main_label
     global current_img
+    global status
 
     current_img-=1
 
@@ -51,6 +64,12 @@ def back():
     main_label.grid_forget()
     main_label = Label(root, image=image_list[current_img])
     main_label.grid(row = 0, column = 0, columnspan = 3)
+    
+    status = Label(root, text="image "+ str(current_img+1) +" of "+ length, bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
+status = Label(root, text="image 0 of "+ str(length), bd=1, relief=SUNKEN, anchor=E)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 button_back = Button(root, text="<<", command=back)
 button_exit = Button(root, text="exit", command=root.quit)
@@ -58,9 +77,10 @@ button_forward = Button(root, text=">>", command = forward)
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=3)
 
 if __name__ == "__main__":
     create_image_list()
     load_first_image()
+    initial_status()
     root.mainloop()
